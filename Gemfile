bf461512d527b4d01e5964f474c5c4bdda13f895
Brett Huneycutt <brett@1000memories.com>
2014-01-22 19:35:40 -0600

    display a simple google map on '/places'

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "https://rubygems.org"
 ruby "2.1.0"
 gem 'sinatra'
 gem 'haml'
+gem 'sinatra-contrib', :require => 'sinatra/content_for'
--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
+require 'rubygems'
+require 'bundler'
 require 'sinatra'
+
+ENV['RACK_ENV'] ||= 'development'
+ENV['MAPS_API_KEY'] ||= 'AIzaSyBuVzKxLXD2UehCt1qfFsTItGe2gHFQvT0'
+
+Bundler.require :default, ENV['RACK_ENV'].to_sym
+
+helpers Sinatra::ContentFor
 
 configure do
   set :views, 'views'
